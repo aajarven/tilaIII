5a3d93aa1d8be8e82d2af32e00fe923ed375f5e7
aajarven <anni.jarvenpaa@gmail.com>
2017-01-24 17:41:48 +0200

    parhaan hypoteesin mukainen, funktioiden output vielä plottaustyyppinen

--- viikko1/ex4-expfuns/exp_funs.c
+++ viikko1/ex4-expfuns/exp_funs.c
@@ -5,6 +5,39 @@ void exp_funs(double x){
     double a = (exp(x)-1)/x;
     double b = (exp(x)-exp(-x))/(2*x);
 
-    printf("a) f1(%.6f)=%.6f\n", x, a)
-    printf("b) f2(%.6f)=%.6f\n", x, b)
+     printf("%.6f\t%.6f\t%.6f\n", x, a, b);
+//    printf("a) f1(%.6f)=%.6f\n", x, a);
+//    printf("b) f2(%.6f)=%.6f\n", x, b);
+}
+
+void exp_funs_Taylor(double x){
+    double a = 0;
+    double prev_a;
+    double i = 1;
+    double factorial = 1; // current denominator
+    do{
+        prev_a = a;
+        factorial *= i;
+        a += pow(x, i-1)/factorial;
+        i++;
+    } while (a!=prev_a);
+
+    a = a;
+
+    double b = 0;
+    double prev_b = 1;
+    i = 1;
+    factorial = 1;
+    do{
+        prev_b = b;
+        factorial *= (i+1)*(i+2);
+        b += pow(x, i-1)/factorial;
+        i += 2;
+    } while (b != prev_b);
+
+    b = b;
+
+     printf("%.6f\t%.6f\t%.6f\n", x, a, b);
+//    printf("a) f1(%.6f)=%.6f\n", x, a);
+//    printf("b) f2(%.6f)=%.6f\n", x, b);
 }
--- viikko1/ex4-expfuns/main.c
+++ viikko1/ex4-expfuns/main.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
-#include "harmonic.h"
+#include "exp_funs.h"
 
 int main(){
-    printf("harmonic: %.6f\n", harmonic());
-
-    printf("harmonic_bunch 50: %.6f\n", harmonic_bunch(50));
-    printf("harmonic_bunch 100: %.6f\n", harmonic_bunch(100));
-    printf("harmonic_bunch 500: %.6f\n", harmonic_bunch(500));
+    for (double i=-0.5; i<=0.5; i+=0.001){
+        exp_funs(i);
+        //exp_funs_Taylor(i);
+    }
+    return 0;
 }
