1afbfb0c0e2211d4180df39ff9534b288bacff35
aajarven <anni.jarvenpaa@gmail.com>
2017-02-28 02:37:30 +0200

    ei vieläkään toimi, näyttää enemmän esimerkkikoodilta, tulostekommentit pois

--- viikko6/ex3-der/re_der.c
+++ viikko6/ex3-der/re_der.c
@@ -6,29 +6,24 @@
 double re_der(double x){
    int N=5;
    double h=0.1;
-   double *D = malloc((N+1)*(N+1)*sizeof(double));
+   double D[N][N];
 
    for(int i=0; i<=N; i++){
-       D[i*(N+1)]=(f(x+h)-f(x-h))/(2*h);
-//       printf("%.6f\t", D[i*N]);
+       D[i][0]=(f(x+h)-f(x-h))/(2*h);
        for(int j=0; j<i-1; j++){
-//           printf("(%d, %d), (%d, %d), (%d, %d)\n", i, j+1, i, j, (i-1), j);
-//           printf("(%d, %d)\n", i, j);
-           D[i*(N+1)+j+1] = D[i*(N+1)+j] + (D[i*(N+1)+j]-D[(i-1)*(N+1)+j])/(pow(4.0,j+1.0)-1.0);
-//           printf("%.6f\t", D[i*N+j+1]);
+           D[i][j+1] = D[i][j] + (D[i][j]-D[i-1][j])/(pow(4.0,j+1.0)-1.0);
        }
-//       printf("\n");
        h = h/2;
    }
 
     for (int i=0; i<N; i++){
         for(int j=0; j<i; j++){
-            printf("%.10f\t", D[i*(N+1)+j]);
+            printf("%.10f\t", D[i][j]);
         }
         printf("\n");
     }
 
-   return D[(N+1)*(N+1)];
+   return D[N-1][N-2];
 }
 
 double f(double x){
