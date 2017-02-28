51c6711dc3424ed5c869e188eefa228c95ac43b0
aajarven <anni.jarvenpaa@gmail.com>
2017-02-28 03:12:53 +0200

    toiminee, vaatii D:n sivun pituudeksi N+1???

--- viikko6/ex3-der/re_der.c
+++ viikko6/ex3-der/re_der.c
@@ -6,24 +6,26 @@
 double re_der(double x){
    int N=5;
    double h=0.1;
-   double D[N][N];
+   double D[N+1][N+1];
 
    for(int i=0; i<=N; i++){
        D[i][0]=(f(x+h)-f(x-h))/(2*h);
-       for(int j=0; j<i-1; j++){
+//       printf("(%d, %d)\n", i, 0);
+       for(int j=0; j<=i-1; j++){
            D[i][j+1] = D[i][j] + (D[i][j]-D[i-1][j])/(pow(4.0,j+1.0)-1.0);
+ //          printf("(%d, %d) (%d, %d) (%d, %d) \n", i, j+1, i, j, i-1, j);
        }
        h = h/2;
    }
 
-    for (int i=0; i<N; i++){
-        for(int j=0; j<i; j++){
-            printf("%.10f\t", D[i][j]);
-        }
-        printf("\n");
-    }
+//    for (int i=0; i<=N; i++){
+//        for(int j=0; j<=i; j++){
+//            printf("%.10f\t", D[i][j]);
+//        }
+//        printf("\n");
+//    }
 
-   return D[N-1][N-2];
+   return D[N][N];
 }
 
 double f(double x){
