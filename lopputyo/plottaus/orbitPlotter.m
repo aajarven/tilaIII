d22669fde09fcb9fca963e79cc47ae5aa6fca928
aajarven <anni.jarvenpaa@gmail.com>
2017-05-29 18:04:20 +0300

    kääntö toimii, en tiedä miksi -pi vaaditaan

--- lopputyo/plottaus/orbitPlotter.m
+++ lopputyo/plottaus/orbitPlotter.m
@@ -1,15 +1,42 @@
-function orbitPlotter(x, y)
+function orbitPlotter(x, y, rotate)
+
+    copyX = x;
+    copyY = y;
 
     colors = [255 0 0; 255, 119, 51; 0 0 0]/255;
-    sizes = [40; 20; 2];
+    sizes = [40; 20; 100];
     markers = ['*', 'o', '.'];
     nBodies = 3;
 
     hold on;
     
+%     if rotate
+%         for i=1:length(x)/nBodies
+%             rotAngle = -atan2(y((i-1)*nBodies+2), x((i-1)*nBodies+2));
+%             for j=1:nBodies
+%                 copyX(i) = cos(rotAngle)*x((i-1)*nBodies+j) - sin(rotAngle)*y((i-1)*nBodies+j);
+%                 copyY(i) = sin(rotAngle)*x((i-1)*nBodies+j) + cos(rotAngle)*y((i-1)*nBodies+j);
+%             end
+%         end
+%     end
+        
     for i=1:nBodies
-        bodyX = x(i:nBodies:length(x));
-        bodyY = y(i:nBodies:length(y));
+        bodyX = copyX(i:nBodies:length(x));
+        bodyY = copyY(i:nBodies:length(y));
+        
+        if rotate
+            if i==1
+                rotAngles = -atan2(bodyY, bodyX)-pi;
+            end
+            
+            for j=1:length(bodyX)
+                newX = cos(rotAngles(j))*bodyX(j) - sin(rotAngles(j))*bodyY(j);
+                newY = sin(rotAngles(j))*bodyX(j) + cos(rotAngles(j))*bodyY(j);
+                bodyX(j) = newX;
+                bodyY(j) = newY;
+            end
+        end
+        
         scatter(bodyX, bodyY, sizes(i), colors(i,:), markers(i))
     end
     
