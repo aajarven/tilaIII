ea57cc0d2ad94660297c5400aecdae364e1c308b
aajarven <anni.jarvenpaa@gmail.com>
2017-05-29 15:42:20 +0300

    parempi lähestymistapa

--- lopputyo/plottaus/orbitPlotter.m
+++ lopputyo/plottaus/orbitPlotter.m
@@ -1,31 +1,22 @@
 function orbitPlotter(x, y)
 
-    colors = [204 0 153; 255 0 0; 0 0 0]/255;
-    colorsRepeated = ones(length(x), 3);
-    sizes = [20; 5; 2];
-    sizesRepeated = ones(length(x), 1);
-
-    for i=1:length(x)
-        sizesRepeated(i) = sizes(mod(i-1, 3)+1);
-
-        for j=1:3
-            colorsRepeated(i,j) = colors(mod(i-1,3)+1,j);
-        end
-
-    end
-
-    scatter(x, y, sizesRepeated, colorsRepeated, 'filled')
-
-    xlabel('x');
-    ylabel('y');
+    colors = [255 0 0; 255, 119, 51; 0 0 0]/255;
+    sizes = [40; 20; 2];
+    markers = ['*', 'o', '.'];
+    nBodies = 3;
 
     hold on;
-    h = zeros(3, 1);
-    for i = 1:3
-        colors(i,:)
-        h(i) = scatter(NaN, NaN, 20, colors(i,:), 'filled');
+    
+    for i=1:nBodies
+        bodyX = x(i:nBodies:length(x));
+        bodyY = y(i:nBodies:length(y));
+        scatter(bodyX, bodyY, sizes(i), colors(i,:), markers(i))
     end
-    legend(h, 'Sun','Jupiter', 'lol');
+    
+    legend('Sun','Jupiter', 'test mass');
+    
+    xlim([-1.1 1.1]);
+    ylim([-1.1 1.1]);
+    pbaspect([1 1 1]);
     hold off;
 end
-
