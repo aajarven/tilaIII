b4a77e4341b864a588093ee3ed0cd3a340f0c16b
aajarven <anni.jarvenpaa@gmail.com>
2017-05-29 13:38:38 +0300

    rakenne speksin mukaiseksi

---
+++ lopputyo/plottaus/orbitPlotter.m
@@ -0,0 +1,30 @@
+function orbitPlotter(x, y)
+
+    colors = [204 0 153; 255 0 0; 255 204 0]/255;
+    colorsRepeated = ones(length(x), 3);
+    sizes = [2; 2; 2];
+    sizesRepeated = ones(length(x), 1);
+
+    for i=1:length(x)
+        sizesRepeated(i) = sizes(mod(i-1, 3)+1);
+
+        for j=1:3
+            colorsRepeated(i,j) = colors(mod(i-1,3)+1,j);
+        end
+
+    end
+
+    scatter(x, y, sizesRepeated, colorsRepeated, 'filled')
+
+    xlabel('x');
+    ylabel('y');
+
+    hold on;
+    h = zeros(3, 1);
+    for i = 1:3
+        h(i) = scatter(NaN, NaN, 20, colors(i,:), 'filled');
+    end
+    legend(h, 'Sun','Jupiter','Test mass');
+    hold off;
+end
+
